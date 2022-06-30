6dd90a2ca8f40324cf0d3bab00666a3c57b7f934
reza2boyce <reza2boyce@gmail.com>
2022-07-01 01:17:02 +0430

    fix respansive video frame

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,68 +1,22 @@
+import "./Home.css";
+
 const Home = () => {
   return (
     <div className="home">
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
-      </section>
-      <section className="section">
-        <h2>title 1</h2>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium,
-          iure ex saepe asperiores ducimus obcaecati? Cupiditate maiores vero
-          asperiores, doloremque, quam adipisci id, itaque possimus ullam a
-          perferendis commodi reiciendis!
-        </p>
+      <section className="split video-section">
+        <div className="split-frame">
+          <div id="theater">
+            <video id="video" src="/video/vfx_01.mp4"></video>
+            <canvas id="canvas"></canvas>
+            <label>
+              <br />
+              try to play me
+            </label>
+          </div>
+        </div>
+        <div className="split-frame">
+          <div>this is munu</div>
+        </div>
       </section>
     </div>
   );
